87543e11a871ddbf52efe7d37c791ff394f68586
James Batsell <jamesbatsell05@gmail.com>
2019-09-18 16:25:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,8 @@
-def my_collect(array)
-  collect = []
-  array.each { |a| collect << yield(a) }
-  collect
-end
-collect
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
